b4a6d2a910d2b4e338deaf6dcd302c0e6688f584
E.Capra <capraenrico13@gmail.com>
2020-03-23 12:40:36 +0100

    /.Implemented Highlighting System
    TODO: Positioning System

---
+++ Assets/Scripts/HighlightingSystem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class HighlightingSystem : MonoBehaviour
+{
+    #region Public Variables
+
+    public Color hoverColor;
+
+    #endregion
+
+    #region Private Variables
+
+    [SerializeField]
+    private MeshRenderer _rend;
+    private Color _startColor;
+
+    #endregion
+
+    #region Init
+
+    private void Awake()
+    {
+        _startColor = _rend.material.color;
+    }
+
+    #endregion
+
+    #region Private Variables
+
+    private void OnMouseEnter()
+    {
+        _rend.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        _rend.material.color = _startColor;
+    }
+
+    #endregion
+}
